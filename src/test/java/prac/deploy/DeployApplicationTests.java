8cc843d087f4952d5a76a52ec18a705edf8c8ccc
veronees <jsg6854@naver.com>
2024-06-27 20:04:29 +0900

    first commit

---
+++ src/test/java/prac/deploy/DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
